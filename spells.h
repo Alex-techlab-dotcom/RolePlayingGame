bf2111438e84bcccd6900f25b41b72a33b72296b
TasosKontos <tasoskontos10@gmail.com>
2020-12-28 00:08:22 +0200

    spells.cpp \ spells.h

---
+++ spells.h
@@ -0,0 +1,42 @@
+#ifndef OURGAME_SPELLS_H
+#define OURGAME_SPELLS_H
+
+#include <iostream>
+#include <string>
+
+class Spell{ //MAKE IT ABSTRACT
+protected:
+    std::string name;
+    int price;
+    int min_level;
+    int mana_cost;
+    int min_damage;
+    int max_damage;
+
+    Spell(std::string, int, int, int, int, int);
+};
+
+//debuffs are given as positive real numbers from 0 to 1 that translate to percentages (ex armor_debuff=0.1 -10% effect)
+
+class IceSpell :public Spell{
+private:
+    double dexterity_debuff;
+    int rounds;
+public:
+    IceSpell(std::string, int, int, int, int, int, double, int);
+};
+
+class FireSpell : public Spell{
+    double armor_debuff;
+    int rounds;
+public:
+    FireSpell(std::string, int, int, int, int, int, double, int);
+};
+
+class LightingSpell : public Spell{
+    double agility_debuff;
+    int rounds;
+public:
+    LightingSpell(std::string, int, int, int, int, int, double, int);
+};
+#endif
